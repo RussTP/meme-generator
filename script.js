a1a9df9159dee06fc694aad747442429300b783c
RussTP <rpawlett@live.com>
2025-06-11 14:59:23 -0400

    added more memes and captions, fixed sizing for mobile

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ const memeArray = [
   "https://i.pinimg.com/736x/00/5b/4f/005b4f9314a589e69fd17a7b82cc4b92.jpg",
   "https://i.pinimg.com/736x/95/d1/b4/95d1b412ac2dacb94282aadf245d53be.jpg",
   "https://i.pinimg.com/736x/6e/42/a9/6e42a90e6d19a027a19b85e9c388fe5e.jpg",
-  "https://i.pinimg.com/736x/7a/63/b8/7a63b83caaffb35c14300e358a617722.jpg"
+  "https://i.pinimg.com/736x/7a/63/b8/7a63b83caaffb35c14300e358a617722.jpg",
+  "https://images.cdn-files-a.com/uploads/1918596/800_5c677085bc984.jpg",
+  "https://images.cdn-files-a.com/uploads/1918596/800_5c678080c0d1d.jpg",
+  "https://images.cdn-files-a.com/uploads/1918596/800_5c678080c0d1d.jpg",
+  "https://images.cdn-files-a.com/uploads/1918596/800_5c6f7c7d6b139.jpeg",
+  "https://images.cdn-files-a.com/uploads/1918596/800_5c6f7cbca7484.jpeg",
+  
+
 
 ];
 
@@ -26,7 +33,13 @@ const captionArray = [
   "Me, hyperfocused on organizing paperclips instead of doing my actual job.",
   "Cannot be toxic if you are emotionally unavailable.",
   "Me: I should go outside. Brain: You will just get sunburn and regret.",
-  "My last two brain cells doing the Macarena during a crisis."
+  "My last two brain cells doing the Macarena during a crisis.",
+  "I do not always lose things.. but when I do, it's the things I am holding",
+  "That moment when you laugh, then immediately wonder if that was your last bit of serotonin.",
+  "I am not dead inside. There is just a long wait list",
+  "They said follow your dreams - I went back to bed",
+
+
 ];
 
 const randomMeme = document.getElementById("random-meme");
